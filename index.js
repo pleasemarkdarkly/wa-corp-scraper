d2857b99930768b14ecca5fe4f1990d29ef40657
pleasemarkdarkly <pretty@pleasemarkdarkly.com>
2020-06-24 00:25:20 -0700

    package

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log("fetch 100 first and last of designated business types");
 
 /*
 TODO: Update CorporationBasicRawStream to accept -1 (for all records) and BusinessType object to crawl all filings of each BusinessType. Add ms timing and color logging.
-      Adding the Filing PDF (https://www.npmjs.com/package/textract) not the pdfparse, timing and a summary output. Example function,
+      Adding the Filing PDF (https://www.npmjs.com/package/textract), timing and a summary output. Example function,
 
     const WA_CORPORATION_ALL = new CorporationBasicRawStream(-1, 1, 1, BusinessType, businessSearchCriteria);
     WA_CORPORATION_ALL._read();
@@ -51,7 +51,6 @@ TODO: Update CorporationBasicRawStream to accept -1 (for all records) and Busine
     Business Type: Scanned 32000 out of 32000, taking 200ms per entity. 50211 total PDF scanned. 
     Total entities 1,020,092 scanned taking 6h:01m:12 an average of .200ms per. 
 */
-
 
 // BusinessType_WA_LIMITED_LIABILITY_CORPORATION
 const WA_LIMITED_LIABILITY_CORPORATION = new CorporationBasicRawStream(100, 1, 1, 65, businessSearchCriteria);
