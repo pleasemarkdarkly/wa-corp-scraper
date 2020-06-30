96b44892a6166cc1c89d325f2e5f2addc9d95d3a
pleasemarkdarkly <pretty@pleasemarkdarkly.com>
2020-06-30 05:29:17 -0700

    cleaning up

--- packages/models/src/entities/entities.model.ts
+++
@@ -1,4 +0,0 @@
-import { model } from "mongoose";
-import { IEntityDocument } from "./entities.types";
-import EntitiesSchema from "./entities.schema";
-export const EntityModel = model<IEntityDocument>("entity", EntitiesSchema);
